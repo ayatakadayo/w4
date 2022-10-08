9299210622458e053f237b741a54deed9b8880c6
ayatakadayo <113568955+ayatakadayo@users.noreply.github.com>
2022-10-08 15:11:48 +0900

    Update silly-story-generator.js

--- path/silly-story-generator.js
+++ path/silly-story-generator.js
@@ -33,8 +33,8 @@ function result() {
   }
   
   if (document.getElementById("us").checked) {
-    const weight = `110ポンド`;
-    const temperature =  `30度(華氏)`;
+    const weight = '110ポンド';
+    const temperature =  '30度(華氏)';
     newStory = newStory.replace('-1度(摂氏)', temperature);
     newStory = newStory.replace('50キロ', weight);
   }
